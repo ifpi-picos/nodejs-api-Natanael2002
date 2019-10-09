1754eb0358de8fd05d96e2d638cc65af8c8ed1bf
RickelmiCristiane123 <aaneclaire@gmail.com>
2019-10-09 12:09:25 -0300

    organizacao e inicio

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const routes = require('./routes');
+
+
+app.use(bodyParser.json());
+app.use('/', routes);
+app.listen(3000,()=>console.log('app online ...'));
